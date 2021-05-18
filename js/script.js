f03f999f507b65c250e2dc3e4f8ef66ecfe9c656
Karabo Pheko <216021429@student.uj.ac.za>
2021-05-18 18:28:34 +0200

    Added some blog posts stuff for realistic content

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var d = new Date();
 document.getElementById('post-date-author').innerHTML = d;
 
+//Log in
 var attempt = 3;
 function validate() {
     var username = document.getElementById('floatingInput').value;
@@ -45,7 +46,7 @@ function resetAll() {
 //WELCOME MESSAGE AFTER LOGGING IN
 window.onload = function () {
     if(localStorage){
-        if(document.getItem('floatingInput')) {
+        if(localStorage.getItem('floatingInput')) {
             document.getElementById('welcomeMessage').textContent = "Hello " + localStorage.getItem('floatingInput');
         } else{
             location.href = './login.html';
@@ -68,3 +69,131 @@ function preview_image(event){
     }
     reader.readAsDataURL(event.target.files[0]);
 }
+
+
+//CONTACT ME PAGE FOR VALIDATION
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+  
+  function forwardMessage(){
+     
+        alert('Your Message has been submitted. You will here from us soon.');
+     
+      
+  }
+
+  //READ MORE FUNCTION
+  function myReadmoreFunction1() {
+    var dots = document.getElementById("param1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myReadmoreFunction2() {
+    var dots = document.getElementById("param2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myReadmoreFunction3() {
+    var dots = document.getElementById("param3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myReadmoreFunction4() {
+    var dots = document.getElementById("param4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myReadmoreFunction5() {
+    var dots = document.getElementById("param5");
+    var moreText = document.getElementById("more5");
+    var btnText = document.getElementById("myBtn5");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myReadmoreFunction6() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
